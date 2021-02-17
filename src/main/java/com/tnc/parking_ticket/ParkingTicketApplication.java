2d68e86b74f4764df76b1c77177eaa120366f23d
TncBogdan <tncbogdan@gmail.com>
2021-02-17 21:12:30 +0200

    first push

---
+++ src/main/java/com/tnc/parking_ticket/ParkingTicketApplication.java
@@ -0,0 +1,13 @@
+package com.tnc.parking_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnc/parking_ticket/ParkingTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnc.parking_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
